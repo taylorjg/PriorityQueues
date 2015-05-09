4c5ccae4644fe2e572fa802842d42b601525477b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-05-09 13:03:51 +0100

    Added more property tests (one of them fails!)

--- PropertyTests.hs
+++ PropertyTests.hs
@@ -22,48 +22,76 @@ insertValues xs =
         loop (x:rest) h = loop rest (insert x h)
         loop _ h = h
 
+getValues :: BinomialQueue Int -> [Int]
+getValues h =
+    reverse $ loop h []
+    where
+        loop h xs = 
+            if isEmpty h
+                then xs
+                else
+                    loop h2 (x:xs)
+                    where
+                        x = findMin h
+                        h2 = deleteMin h
+
+isOrderedCorrectly :: BinomialQueue Int -> Bool
+isOrderedCorrectly h =
+    case tryGetMin h of
+        (True, m) -> loop h m
+        _ -> True
+    where
+        tryGetMin h =
+            case isEmpty h of
+                False -> (True, findMin h)
+                True -> (False, 0)
+        loop h previousMin =
+            case tryGetMin h2 of
+                (True, m) -> m >= previousMin && loop h2 m
+                _ -> True
+            where
+                h2 = deleteMin h
+
 prop_FindMinReturnsOnlyItem :: Int -> Bool
-prop_FindMinReturnsOnlyItem a = 
-    let h = insert a empty
-    in findMin h == a
+prop_FindMinReturnsOnlyItem x = 
+    findMin h == x
+    where
+        h = insert x empty
 
 prop_DeleteMinDeletesOnlyItem :: Int -> Bool
-prop_DeleteMinDeletesOnlyItem a = 
-    let
-        h1 = insert a empty
+prop_DeleteMinDeletesOnlyItem x = 
+    isEmpty h2
+    where
+        h1 = insert x empty
         h2 = deleteMin h1
-    in
-        isEmpty h2
 
 prop_FindMinReturnsMinOfTwoItems :: Int -> Int -> Bool
-prop_FindMinReturnsMinOfTwoItems a b = 
-    let
-        h1 = insert a empty
-        h2 = insert b h1
-    in
-        findMin h2 == min a b
+prop_FindMinReturnsMinOfTwoItems x y = 
+    findMin h2 == min x y
+    where
+        h1 = insert x empty
+        h2 = insert y h1
+
+prop_IsOrderedCorrectly :: [Int] -> Bool
+prop_IsOrderedCorrectly xs =
+    isOrderedCorrectly h
+    where
+        h = insertValues xs
+
+-- This test currently fails when xs contains the
+-- same value repeated >= 6 times (fine for 5 times)
+prop_InsertingTheSameValueSeveralTimes :: Int -> Int -> Bool
+prop_InsertingTheSameValueSeveralTimes n x = 
+    getValues h == xs
+    where
+        l = abs n `mod` 10
+        xs = replicate l x
+        h = insertValues xs
 
 main :: IO ()
 main = do
     quickCheck prop_FindMinReturnsOnlyItem
     quickCheck prop_DeleteMinDeletesOnlyItem
     quickCheck prop_FindMinReturnsMinOfTwoItems
-
-    --let h1 = empty
-    --putStrLn $ show $ isEmpty h1
-
-    --let h2 = insert 10 h1
-    --putStrLn $ show $ isEmpty h2
-    --putStrLn $ show $ findMin h2
-
-    --let h3 = insert 11 h2
-    --putStrLn $ show $ isEmpty h3
-    --putStrLn $ show $ findMin h3
-
-    --let h4 = insert 5 h2
-    --putStrLn $ show $ isEmpty h4
-    --putStrLn $ show $ findMin h4
-
-    --let h5 = deleteMin h4
-    --putStrLn $ show $ isEmpty h5
-    --putStrLn $ show $ findMin h5
+    quickCheck prop_IsOrderedCorrectly
+    quickCheck prop_InsertingTheSameValueSeveralTimes
